b6afc86ba01c94b59b99d06f9ff9483dcaf098be
carlos349 <carlos.gomes349@gmail.com>
2018-05-11 17:40:24 +0200

    finished empresa_page

--- static_my_proj/js/main.js
+++ static_my_proj/js/main.js
@@ -1,37 +1,37 @@
-const Slider = document.querySelector('.items');
-let isDown = false;
-let startX;
-let scrollLeft;
-
-Slider.addEventListener('mousedown', (e) => {
-  isDown = true;
-  Slider.classList.add('active');
-  startX = (e.screenX - Slider.offsetLeft);
-  scrollLeft = Slider.scrollLeft;
-
-});
-
-Slider.addEventListener('mouseleave', () => {
-  isDown = false;
-  Slider.classList.remove('active');
-});
-
-Slider.addEventListener('mouseup', () => {
-  isDown = false;
-  Slider.classList.remove('active');
-});
-
-Slider.addEventListener('mousemove', (e) => {
-  if(!isDown) return;
-  e.preventDefault();
-  const x = e.screenX - Slider.offsetLeft;
-  const walk = x - startX;
-
-  Slider.scrollLeft = scrollLeft - walk;
-});
-
-
 $(document).ready(function(){
+
+    const Slider = document.querySelector('.items');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    Slider.addEventListener('mousedown', (e) => {
+      isDown = true;
+      Slider.classList.add('active');
+      startX = (e.screenX - Slider.offsetLeft);
+      scrollLeft = Slider.scrollLeft;
+
+    });
+
+    Slider.addEventListener('mouseleave', () => {
+      isDown = false;
+      Slider.classList.remove('active');
+    });
+
+    Slider.addEventListener('mouseup', () => {
+      isDown = false;
+      Slider.classList.remove('active');
+    });
+
+    Slider.addEventListener('mousemove', (e) => {
+      if(!isDown) return;
+      e.preventDefault();
+      const x = e.screenX - Slider.offsetLeft;
+      const walk = x - startX;
+
+      Slider.scrollLeft = scrollLeft - walk;
+    });
+
 
    $('.boton').on('click', function(){
 
@@ -44,4 +44,5 @@ $(document).ready(function(){
     }
 
    });
+
 });
