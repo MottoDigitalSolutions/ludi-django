2c8de04d764d312a925465e7c4e83bfdf1eeaa95
carlos349 <carlos.gomes349@gmail.com>
2018-05-21 22:53:19 +0200

    integrating prevencion_page

--- static_my_proj/js/main.js
+++ static_my_proj/js/main.js
@@ -1,5 +1,11 @@
-
-//nosotros filosofias
+/* MAP JS */
+function myMap() {
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: new google.maps.LatLng(51.5, -0.2), zoom: 10
+  };
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+}
 
 $(document).ready(function(){
 		// set up hover panels
