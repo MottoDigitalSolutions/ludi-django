7e3685cef54530754687f2223252e8d5648022b2
carlosblanco <carlosblancosierra@gmail.com>
2018-05-22 10:53:58 -0500

    implemented: checkups url, templates, nav

--- ludi/urls.py
+++ ludi/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import home_page, promociones_page, empresas_page, nosotros_page, contacto_page, prevencion_page
-
+from .views import home_page, promociones_page, empresas_page, nosotros_page, contacto_page, prevencion_page, \
+    que_es_page, empresarial_page, integral_page, plus_page, genetest_page, especialidad_page
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -29,7 +29,15 @@ urlpatterns = [
     url(r'^empresas/$', empresas_page, name='empresas'),
     url(r'^nosotros/$', nosotros_page, name='nosotros'),
     url(r'^contacto/$', contacto_page, name='contacto'),
-    url(r'^prevencion/$', prevencion_page, name='prevencion')
+    # url(r'^prevencion/$', prevencion_page, name='prevencion'),
+    url(r'^check-ups/prevencion/$', prevencion_page, name='prevencion'),
+    url(r'^check-ups/que-es-un-check-up/$', que_es_page, name='que_es'),
+    url(r'^check-ups/check-up-empresarial/$', empresarial_page, name='empresarial'),
+    url(r'^check-ups/check-up-integral/$', integral_page, name='integral'),
+    url(r'^check-ups/check-up-plus/$', plus_page, name='plus'),
+    url(r'^check-ups/check-up-genetest/$', genetest_page, name='genetest'),
+    url(r'^check-ups/check-up-especialidad/$', especialidad_page, name='especialidad'),
+
 ]
 
 if settings.DEBUG:
--- ludi/views.py
+++ ludi/views.py
@@ -7,10 +7,10 @@ def home_page(request):
         "content": "",
     }
 
-    return render(request,"home_page.html", context)
+    return render(request, "home_page.html", context)
+
 
 def promociones_page(request):
-
     context = {
         "title": "Ludi | Promociones",
         "content": "Welcome to the hompage",
@@ -18,8 +18,8 @@ def promociones_page(request):
 
     return render(request, "promociones_page.html", context)
 
-def empresas_page(request):
 
+def empresas_page(request):
     context = {
         "title": "Ludi | Empresas",
         "content": "Welcome to the hompage",
@@ -27,8 +27,8 @@ def empresas_page(request):
 
     return render(request, "empresas_page.html", context)
 
-def nosotros_page(request):
 
+def nosotros_page(request):
     context = {
         "title": "Ludi | Nosotros",
         "content": "Welcome to the hompage",
@@ -36,8 +36,8 @@ def nosotros_page(request):
 
     return render(request, "nosotros_page.html", context)
 
-def contacto_page(request):
 
+def contacto_page(request):
     context = {
         "title": "Ludi | Contacto",
         "content": "Welcome to the hompage",
@@ -45,11 +45,65 @@ def contacto_page(request):
 
     return render(request, "contacto_page.html", context)
 
-def prevencion_page(request):
 
+def prevencion_page(request):
     context = {
         "title": "Ludi | Prevencion",
         "content": "Welcome to hompage",
     }
 
     return render(request, "prevencion_page.html", context)
+
+
+def que_es_page(request):
+    context = {
+        "title": "Ludi | Prevencion",
+        "content": "Welcome to hompage",
+    }
+
+    return render(request, "que_es_page.html", context)
+
+
+def empresarial_page(request):
+    context = {
+        "title": "Ludi | Prevencion",
+        "content": "Welcome to hompage",
+    }
+
+    return render(request, "empresarial_page.html", context)
+
+
+def integral_page(request):
+    context = {
+        "title": "Ludi | Prevencion",
+        "content": "Welcome to hompage",
+    }
+
+    return render(request, "integral_page.html", context)
+
+
+def plus_page(request):
+    context = {
+        "title": "Ludi | Prevencion",
+        "content": "Welcome to hompage",
+    }
+
+    return render(request, "plus_page.html", context)
+
+
+def genetest_page(request):
+    context = {
+        "title": "Ludi | Prevencion",
+        "content": "Welcome to hompage",
+    }
+
+    return render(request, "genetest_page.html", context)
+
+
+def especialidad_page(request):
+    context = {
+        "title": "Ludi | Prevencion",
+        "content": "Welcome to hompage",
+    }
+
+    return render(request, "especialidad_page.html", context)
